e278906c9c396757953b8e4815a5119b8a7703fe
alysonbennett <alysonbennett@gmai.com>
2021-02-25 19:47:06 -0500

    added Item.js to models

---
+++ models/Item.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const ItemSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  breed: {
+    type: String,
+    required: true
+  },
+  weight: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: String,
+    required: true
+  },
+  image: {
+    data: Buffer,
+    contentType: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  userId: {
+    required: true,
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "user"
+  }
+});
+
+module.exports = Item = mongoose.model('item', ItemSchema);
