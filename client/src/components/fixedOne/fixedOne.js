f20d2ea0d9990d35f39b7f8f2b50ad90dd5b4b76
alysonbennett <alysonbennett@gmai.com>
2021-03-02 18:44:09 -0500

    added fixedOne folder to components

---
+++ client/src/components/fixedOne/fixedOne.js
@@ -0,0 +1,14 @@
+import React from "react"; 
+import "./fixedOne.css";
+
+
+//The fixed and scroll 1-3 components are for the fancy fixed scroll effect to render////
+
+
+function FixedOne() {
+    return (
+        <div className="fixed one border-top border-light"></div>
+)
+}
+
+export default FixedOne;
