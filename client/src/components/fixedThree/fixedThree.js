a6a51ed656c12f75a1ce1470b1fa4c69d3021d21
alysonbennett <alysonbennett@gmai.com>
2021-03-02 18:45:06 -0500

    added fixedThree folder to components

---
+++ client/src/components/fixedThree/fixedThree.js
@@ -0,0 +1,13 @@
+import React from "react"; 
+import "./fixedThree.css";
+
+//The fixed and scroll 1-3 components are for the fancy fixed scroll effect to render////
+
+function FixedThree() {
+    return (
+        <div className="fixed three border-top border-light"></div>
+        
+)
+}
+
+export default FixedThree;
