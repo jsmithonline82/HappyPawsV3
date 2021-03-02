5b16d01d21daa1d101ca76c93fa48e9052e0f76d
alysonbennett <alysonbennett@gmai.com>
2021-03-02 18:45:45 -0500

    added fixedTwo folder to components

---
+++ client/src/components/fixedTwo/fixedTwo.js
@@ -0,0 +1,13 @@
+import React from "react"; 
+import "./fixedTwo.css";
+
+
+//The fixed and scroll 1-3 components are for the fancy fixed scroll effect to render////
+
+function FixedTwo() {
+    return (
+        <div className="fixed two border-top border-light"></div>
+)
+}
+
+export default FixedTwo;
