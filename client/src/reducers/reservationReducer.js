ca6cc5b6dacdf40b690ef2a81526fd7c7cc83d52
alysonbennett <alysonbennett@gmai.com>
2021-03-02 19:08:36 -0500

    added reducers folder to scr

---
+++ client/src/reducers/reservationReducer.js
@@ -0,0 +1,39 @@
+import {
+  GET_RESERVATIONS,
+  ADD_RESERVATION,
+  DELETE_RESERVATION,
+  RESERVATIONS_LOADING
+} from '../actions/types';
+
+const initialState = {
+  reservations: [],
+  loading: false
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case GET_RESERVATIONS:
+      return {
+        ...state,
+        reservations: action.payload,
+        loading: false
+      };
+    case DELETE_RESERVATION:
+      return {
+        ...state,
+        reservations: state.reservations.filter(reservation => reservation._id !== action.payload)
+      };
+    case ADD_RESERVATION:
+      return {
+        ...state,
+        reservations: [action.payload, ...state.reservations]
+      };
+    case RESERVATIONS_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    default:
+      return state;
+  }
+}
